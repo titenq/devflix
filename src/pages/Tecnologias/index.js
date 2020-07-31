ede8d32815f88f1e399bd3f57454b78066619da5
titenq <titenq@gmail.com>
2020-07-31 19:47:58 -0300

    form tecnologies

--- src/pages/Tecnologias/index.js
+++ src/pages/Tecnologias/index.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 import styles from './Tecnologias.module.css';
 import Button from '../../components/Button';
+import screenshot from '../../assets/img/screenshot.jpg';
 
 
 const Tecnologias = () => {
   const initialValues = {
     name: '', 
-    color: '#ff0000'
+    color: '#6925D9'
   };
  
   const [values, setValues] = useState(initialValues);
@@ -47,37 +48,42 @@ const Tecnologias = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ backgroundImage: `url(${screenshot})` }}>
       <h1 className={styles.title}>Cadastro de Tecnologia</h1>
 
       <div className={styles.form_container}>
         <form onSubmit={handleSubmit}>
           <div className={styles.form}>
-            <div className={styles.label_float}>
-              <input
-                className={styles.input}
-                type="text" 
-                id="name" 
-                name="name"
-                value={values.name} 
-                placeholder=" "
-                autoComplete="off"
-                required
-                onChange={handleChange} 
-              />
-              <label className={styles.label} htmlFor="name">Nome da Tecnologia</label>
+            <div className={styles.input_container}>
+              <span>Digite a Tecnologia:</span>
+              <div className={styles.label_float}>
+                <input
+                  className={styles.input}
+                  type="text" 
+                  id="name" 
+                  name="name"
+                  value={values.name} 
+                  placeholder=" "
+                  autoComplete="off"
+                  required
+                  onChange={handleChange} 
+                />
+                <label className={styles.label} htmlFor="name">Nome da Tecnologia</label>
+              </div>
             </div>
 
-            <label className={styles.label_color} htmlFor="color">Escolha a Cor:</label>
-            <div className={styles.color_container}>
-            <input
-                className={styles.color}
-                type="color" 
-                id="color" 
-                name="color"
-                value={values.color}
-                onChange={handleChange} 
-              />
+            <div className={styles.input_color_container}>
+              <label className={styles.label_color} htmlFor="color">Escolha a Cor:</label>
+              <div className={styles.color_container}>
+              <input
+                  className={styles.color}
+                  type="color" 
+                  id="color" 
+                  name="color"
+                  value={values.color}
+                  onChange={handleChange} 
+                />
+              </div>
             </div>
           </div>
           
